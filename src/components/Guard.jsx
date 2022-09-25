716f9c6158eadde0f3ffb1bfc1084bcfdd05b23a
Christian Peters <c.peters@makermaker.de>
2022-09-25 19:40:55 +0200

    added guarded route

--- src/components/Guard.jsx
+++ src/components/Guard.jsx
@@ -1,10 +1,26 @@
-import { Outlet } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { Outlet, useNavigate } from "react-router-dom"
 
 const Guard = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const navigate = useNavigate()
+    useEffect(async()=> {
+        const result = await fetch('http://localhost:9898/api/validate', {
+            method: 'POST',
+            credentials: 'include'
+        })
+        const data = await result.json()
+        setIsLoggedIn(data.state)
+    }, [])
+
+    const check = () =>{
+        if(isLoggedIn) return <Outlet></Outlet>
+        navigate('/')
+    }
+
     return (
         <>
-        <div>Hallo</div>
-        <Outlet></Outlet>
+        { check() }
         </>
     )
 }
