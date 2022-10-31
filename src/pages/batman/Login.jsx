3abcb7701e4104b2aa88290da44c4a668fb10248
Christian Peters <c.peters@makermaker.de>
2022-10-31 22:38:27 +0100

    added env to the Login

--- src/pages/batman/Login.jsx
+++ src/pages/batman/Login.jsx
@@ -6,20 +6,21 @@ const Login = (props) => {
     const [pass, setPass] = useState('')
     const navigate = useNavigate()
 
-    const login = () =>{
+    const login = () => {
         console.log('in login');
-        fetch('http://localhost:9898/api/login',{
+        fetch(process.env.REACT_APP_SERVER_BASE_URL + '/api/login', {
             method: 'POST',
             credentials: 'include',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({username:username,pass:pass})
-            }
+            body: JSON.stringify({ username: username, pass: pass })
+        }
         )
-        .then( response => {
-            if(response.status === 200)navigate('/batman/batcave')
-            response.json()})
+            .then(response => {
+                if (response.status === 200) navigate('/batman/batcave')
+                response.json()
+            })
         // .then( data => console.log(data))
         // .catch(err => console.log(err))
     }
@@ -27,7 +28,7 @@ const Login = (props) => {
         <main className="bg-mainpurple flex h-screen justify-center">
             <section className="bg-white self-center w-1/4 p-5 flex  flex-col">
                 <label className="" htmlFor="">Anmeldename</label>
-                <input className="outline outline-1 text-xl py-1" type="text" onChange={(e)=>setUsername(e.target.value)}/>
+                <input className="outline outline-1 text-xl py-1" type="text" onChange={(e) => setUsername(e.target.value)} />
                 <label className="mt-5">Passwort</label>
                 <input className="outline outline-1 text-xl py-1" type="password" onChange={e => setPass(e.target.value)} />
                 <button className="bg-mainpurple mt-5 py-2 text-white" onClick={login}>Anmelden</button>
