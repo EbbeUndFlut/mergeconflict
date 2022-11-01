814db6e335ece157e1510664a189f3ef58468a05
Christian Peters <c.peters@makermaker.de>
2022-11-01 10:53:11 +0100

    changed text on landingpage

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = (props) => {
             <div className="basis-1/2">
                 <h1 className="font-sans font-light text-white text-8xl">Lerne Git,</h1>
                 <p className="font-sans  text-8xl text-secondpurple">wir helfen Dir!</p>
-                <p className="mt-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis neque aspernatur temporibus maxime, culpa similique distinctio nulla? Possimus accusamus voluptatibus quis nesciunt eveniet sit non vitae rem maxime labore, dolorem dolores aperiam, alias eius mollitia ut architecto dolore maiores laudantium inventore soluta enim atque voluptas! Eos quod laboriosam aperiam? Assumenda.</p>
+                <p className="mt-5">Ist git für dich ein Mysterium? Hättest du gerne einen Ort wo Du einfach mal schauen kannst was ein Befehl macht? Wir wollen Dir zeigen was so toll an git ist und warum wir es lieben. Nutze unsere Tutorials oder den schnellen Befehls-Finder. Mit uns wird git eine spassige Angelegenheit.</p>
                 <HeroButton text="Lerne jetzt Git"></HeroButton>
             </div>
             <div></div>
